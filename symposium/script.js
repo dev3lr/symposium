318b5d0749a12e0a4291ac8c1083138992140a13
dev3 <125292402+dev3lr@users.noreply.github.com>
2025-03-05 22:42:43 +0530

    Delete symposium directory

--- symposium/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuBtn = document.querySelector(".menu-btn");
-    const navLinks = document.querySelector(".nav-links");
-
-    menuBtn.addEventListener("click", () => {
-        navLinks.classList.toggle("show");
-    });
-});
-
