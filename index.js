9e2a949b50a7e0dd4e6f32cd46859a6e4cdd3f5d
dev3 <3threetm@gmail.com>
2025-03-05 22:46:39 +0530

    v.1

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuBtn = document.querySelector(".menu-btn");
+    const navLinks = document.querySelector(".nav-links");
+
+    menuBtn.addEventListener("click", () => {
+        navLinks.classList.toggle("show");
+    });
+});
+
