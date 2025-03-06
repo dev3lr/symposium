719b711618798400d4c5924bb840cabdbd5b0151
dev3 <3threetm@gmail.com>
2025-03-07 03:21:47 +0530

    v.0.3

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+
+
 document.addEventListener("DOMContentLoaded", function () {
     const menuBtn = document.querySelector(".menu-btn");
     const navLinks = document.querySelector(".nav-links");
 
-    menuBtn.addEventListener("click", () => {
+    menuBtn.addEventListener("click", (event) => {
         navLinks.classList.toggle("show");
+        event.stopPropagation(); // Prevent click from propagating to document
+    });
+
+    document.addEventListener("click", (event) => {
+        if (!navLinks.contains(event.target) && !menuBtn.contains(event.target)) {
+            navLinks.classList.remove("show");
+        }
     });
 });
-
